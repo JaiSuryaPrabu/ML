6bae5bcb5bd68697ecf008a0174b97418f3b5729
JaiSuryaPrabu <jaisuryaprabakaran25204@gmail.com>
2023-04-07 22:50:54 +0530

    Completed Random forest

---
+++ Supervised_Learning/Classification/random_forest.py
@@ -0,0 +1,43 @@
+# Import the dataset
+import pandas as pd
+
+print("Data is imported ...")
+data = pd.read_csv("ads.csv")
+print("Data in the dataset : ")
+print(data.head())
+
+# Splitting into the dependent and independent variables and testing and training data
+from sklearn.model_selection import train_test_split
+X = data.iloc[:,:-1].values
+y = data.iloc[:,-1].values
+print("Data are splitted into dependent and independent values ...")
+x_train,x_test,y_train,y_test = train_test_split(X,y,test_size=0.25)
+print("Data are splitted into training and testing data ...")
+
+# feature scaling
+print("Features are ready to be scaled down ...")
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+print("Features are scaled ...")
+
+# training model
+print("Training the model ...")
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=50)
+classifier.fit(x_train,y_train)
+print("Random forest model is trained ...")
+pred = classifier.predict(x_test)
+
+# Evaluation
+print("Evaluation of the model ...")
+from sklearn.metrics import accuracy_score,confusion_matrix
+score = accuracy_score(y_test,pred)
+matrix = confusion_matrix(y_test,pred)
+print(f"Accuracy Score : {score} \n Confusion matrix : \n{matrix}")
+
+'''Accuracy Score : 0.91 
+ Confusion matrix :
+[[61  4]
+ [ 5 30]]'''
